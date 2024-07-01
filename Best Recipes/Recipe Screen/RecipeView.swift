bc4e52a5b771cc414dad3c1e16691dd4a7658344
Alexander <17aml08@gmail.com>
2024-07-02 00:11:01 +0300

    update

--- Best Recipes/Recipe Screen/RecipeView.swift
+++ Best Recipes/Recipe Screen/RecipeView.swift
@@ -11,5 +11,29 @@ final class RecipeView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addViews()
+        layoutViews()
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        addViews()
+        layoutViews()
+        configure()
+    }
+}
+
+extension RecipeView {
+    func addViews() {
+        
+    }
+    
+    func layoutViews() {
+        
+    }
+    
+    func configure() {
+        backgroundColor = .red
     }
 }
--- Best Recipes/Recipe Screen/RecipeViewController.swift
+++ Best Recipes/Recipe Screen/RecipeViewController.swift
@@ -9,4 +9,9 @@ import UIKit
 
 final class RecipeViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        l
+    }
 }
