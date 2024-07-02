6879c5d08b85825e274aa48ad552b46a51eb348c
Ivan Semikin <i.semikin.94@icloud.com>
2024-07-02 21:18:10 +0200

    UICollectionView Delegate/DataSource

--- Best Recipes/Home Screen/HomeViewController.swift
+++ Best Recipes/Home Screen/HomeViewController.swift
@@ -9,16 +9,49 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    private let collectionView: UICollectionView = {
+       let collectionViewLayout = UICollectionViewLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout)
+        collectionView.backgroundColor = .none
+        collectionView.bounces = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private let sections = MockData.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
 
 // MARK: - Private Methods
+
 private extension HomeViewController {
-    func setupUI() {
-        
+    func setDelegates() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
+
+// MARK: - UICollectionViewDelegate
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
+
+// MARK: - UICollectionViewDataSource
+
+extension HomeViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        <#code#>
     }
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
 }
