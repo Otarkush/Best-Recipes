9a9c1706f5765ed349f4c08e5ac8dafce972dde5
Ivan Semikin <i.semikin.94@icloud.com>
2024-07-02 20:36:52 +0200

    Create ListItem + ListSection

--- Best Recipes/Home Screen/HomeViewController.swift
+++ Best Recipes/Home Screen/HomeViewController.swift
@@ -5,4 +5,20 @@
 //  Created by Андрей Линьков on 30.06.2024.
 //
 
-import Foundation
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+// MARK: - Private Methods
+private extension HomeViewController {
+    func setupUI() {
+        
+    }
+    
+    
+}
