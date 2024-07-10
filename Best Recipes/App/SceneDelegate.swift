6a511387d1f2f9072daa76e1cec3b9284f967f14
Alexander <17aml08@gmail.com>
2024-07-11 00:35:27 +0300

    setup UI

--- Best Recipes/App/SceneDelegate.swift
+++ Best Recipes/App/SceneDelegate.swift
@@ -15,9 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let appCoordinator = AppCoordinator(type: .onboarding, navigationController: nil)
-        self.coordinator = appCoordinator
-        appCoordinator.window = window
-        appCoordinator.start()
+//        let appCoordinator = AppCoordinator(type: .onboarding, navigationController: nil)
+//        self.coordinator = appCoordinator
+//        appCoordinator.window = window
+//        appCoordinator.start()
+        window?.rootViewController = SeeAllViewController()
+        window?.makeKeyAndVisible()
     }
 }
