9b5fdf36b51732295617e3d75517f6498c6e709b
Alexander <17aml08@gmail.com>
2024-07-11 00:35:27 +0300

    setup UI

---
+++ Best Recipes/Flows/SeeAllFlow/SeeAllScreen/SeeAllViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SeeAllViewController.swift
+//  Best Recipes
+//
+//  Created by Alexander on 10.07.24.
+//
+
+import UIKit
+
+final class SeeAllViewController: UIViewController {
+    
+    //MARK: - Private properties
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.register(RecipeTableViewCell.self, forCellReuseIdentifier: RecipeTableViewCell.description())
+        return tableView
+    }()
+    
+    //MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+       setupUI()
+    }
+}
+
+//MARK: - SetupUI
+
+private extension SeeAllViewController {
+    func setupUI() {
+        view.backgroundColor = .white
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        addViews()
+        setupConstraints()
+    }
+    
+    func addViews() {
+        view.addSubview(tableView)
+    }
+    
+    func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+//MARK: - UITableViewDataSource and UITableViewDelegate
+
+extension SeeAllViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: RecipeTableViewCell.description(), for: indexPath) as? RecipeTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 224
+    }
+}
